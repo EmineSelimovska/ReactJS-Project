755ac3877754e9ed3774d256587cadd4f89fd303
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-11-03 16:22:07 +0200

    Update Slider

--- my-project/src/components/Slider.jsx
+++ my-project/src/components/Slider.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Slide } from "react-slideshow-image"
 import 'react-slideshow-image/dist/styles.css';
+import * as dateService from "../servises/dateService";
 
 
 
@@ -25,83 +26,52 @@ const img = [
 
 
 
-export default function Slider(){
+export default function Slider() {
 
-  return(
-   
-        <section id="aa-slider">
-         
-        <div className="aa-slider-area">
-     
-          <div className="aa-top-slider">
+
+
+
+  return (
+
+    <section id="aa-slider">
+
+      <div className="aa-slider-area">
+
+        <div className="aa-top-slider">
           <Slide {...proprietes}>
-        <div className="aa-top-slider-single">
-              <img src={img[0]} alt="img" />
-            
-             
-            </div>
-           
+
+
             <div className="aa-top-slider-single">
-              <img src={img[1]} alt="img1"/>
-              <div className="aa-top-slider-content">
-                <span className="aa-top-slider-catg">Duplex</span>
-                <h2 className="aa-top-slider-title">1560 Square Feet</h2>
-                <p className="aa-top-slider-location"><i className="fa fa-map-marker" />South Beach, Miami (USA)</p>
-                <span className="aa-top-slider-off">30% OFF</span>
-                <p className="aa-top-slider-price">$460,000</p>
-                <a href="#" className="aa-top-slider-btn">Read More <span className="fa fa-angle-double-right" /></a>
-              </div>
+
+              <img src={img[1]} alt="img1" />
+
+
             </div>
             <div className="aa-top-slider-single">
               <img src={img[2]} alt="img" />
-              <div className="aa-top-slider-content">
-                <span className="aa-top-slider-catg">Duplex</span>
-                <h2 className="aa-top-slider-title">1560 Square Feet</h2>
-                <p className="aa-top-slider-location"><i className="fa fa-map-marker" />South Beach, Miami (USA)</p>
-                <span className="aa-top-slider-off">30% OFF</span>
-                <p className="aa-top-slider-price">$460,000</p>
-                <a href="#" className="aa-top-slider-btn">Read More <span className="fa fa-angle-double-right" /></a>
-              </div>
+
+
             </div>
             <div className="aa-top-slider-single">
               <img src={img[3]} alt="img" />
-              <div className="aa-top-slider-content">
-                <span className="aa-top-slider-catg">Duplex</span>
-                <h2 className="aa-top-slider-title">1560 Square Feet</h2>
-                <p className="aa-top-slider-location"><i className="fa fa-map-marker" />South Beach, Miami (USA)</p>
-                <span className="aa-top-slider-off">30% OFF</span>
-                <p className="aa-top-slider-price">$460,000</p>
-                <a href="#" className="aa-top-slider-btn">Read More <span className="fa fa-angle-double-right" /></a>
-              </div>
+
+
             </div>
             <div className="aa-top-slider-single">
               <img src={img[4]} alt="img" />
-              <div className="aa-top-slider-content">
-                <span className="aa-top-slider-catg">Duplex</span>
-                <h2 className="aa-top-slider-title">1560 Square Feet</h2>
-                <p className="aa-top-slider-location"><i className="fa fa-map-marker" />South Beach, Miami (USA)</p>
-                <span className="aa-top-slider-off">30% OFF</span>
-                <p className="aa-top-slider-price">$460,000</p>
-                <a href="#" className="aa-top-slider-btn">Read More <span className="fa fa-angle-double-right" /></a>
-              </div>
+
+
             </div>
             <div className="aa-top-slider-single">
               <img src={img[5]} alt="img" />
-              <div className="aa-top-slider-content">
-                <span className="aa-top-slider-catg">Duplex</span>
-                <h2 className="aa-top-slider-title">1560 Square Feet</h2>
-                <p className="aa-top-slider-location"><i className="fa fa-map-marker" />South Beach, Miami (USA)</p>
-                <span className="aa-top-slider-off">30% OFF</span>
-                <p className="aa-top-slider-price">$460,000</p>
-                <a href="#" className="aa-top-slider-btn">Read More <span className="fa fa-angle-double-right" /></a>
-              </div>
-            </div>
-            </Slide>
-          </div>
-         
-        </div>
-       
-      </section>
 
-    )
+            </div>
+          </Slide>
+        </div>
+
+      </div>
+
+    </section>
+
+  )
 }
--- my-project/src/servises/dateService.js
+++ my-project/src/servises/dateService.js
@@ -1,11 +1,12 @@
 const baseUrl = `http://localhost:3030/jsonstore/contact`;
+const propUrl = `http://localhost:3030/jsonstore/property`
 
 
 export const getContact = async () => {
     try {
         const response = await fetch(baseUrl);
         const result = await response.json();
-        
+
         const data = Object.values(result);
 
         return data;
@@ -13,5 +14,21 @@ export const getContact = async () => {
         return err
     }
 
-   
+
+}
+
+export const getProperty = async () => {
+    try {
+        const response = await fetch(propUrl);
+        const result = await response.json();
+
+        const date = Object.values(result);
+
+        
+
+       
+        return date;
+    } catch (err) {
+        return err;
+    }
 }
