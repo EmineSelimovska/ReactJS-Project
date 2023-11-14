f9b18d6d43458445dd3d7c564c4ae88fcf5ef23f
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-11-14 14:03:44 +0200

    add create home css
    add propertyType css
    add propertyTypeField

--- my-project/src/components/Create.jsx
+++ my-project/src/components/Create.jsx
@@ -1,7 +1,4 @@
 import { Link } from "react-router-dom";
-import Header from "./Header";
-import Menu from "./Menu-area";
-import Footer from "./Footer";
 
 import  styles from "./Create.module.css"
 export default function Create(){
@@ -15,48 +12,48 @@ export default function Create(){
                         <div className={styles.createArea}>
                             <div className={styles.createForm}>
                                 <div className={styles.formTitle}>
-                                    <Link className="aa-create-home" to="/">Home</Link>
-                                    <h4>Create property</h4>
+                                    <Link className={styles.createHome} to="/">Home</Link>
+                                    <h4 className={styles.h4}>Create property</h4>
                                 </div>
                                 <form className="createform">
-                                    <div className="aa-propertyType-field">
-                                        <label htmlFor="properyType">Property Type </label>
-                                        <input type="propertyType" required="required"   name="propertyType" />
+                                    <div className={styles.propertyTypeField}>
+                                        <label  htmlFor="properyType">Property Type </label>
+                                        <input type="propertyType" required="required" name="propertyType" id="propertyType"/>
                                     </div>
                                     <div className="aa-city-field">
                                         <label htmlFor="city">City</label>
-                                        <input type="city" required="required"  name="city" />
+                                        <input type="city" required="required"  name="city" id="city" />
                                     </div>
                                     <div className="aa-street-field">
                                         <label htmlFor="street">Street </label>
-                                        <input type="street" required="required"   name="street" />
+                                        <input type="street" required="required"   name="street" id="street" />
                                     </div>
                                     <div className="aa-price-field">
                                         <label htmlFor="price">Price </label>
-                                        <input type="price" required="required"  name="price" />
+                                        <input type="price" required="required"  name="price" id="price"/>
                                     </div>
                                     <div className="aa-bedrooms-field">
                                         <label htmlFor="bedrooms">Bedrooms </label>
-                                        <input type="bedrooms" required="required"  name="bedrooms" />
+                                        <input type="bedrooms" required="required"  name="bedrooms" id="bedrooms"/>
                                     </div>
                                     <div className="aa-bathrooms-field">
                                         <label htmlFor="bathrooms">Bathrooms </label>
-                                        <input type="bathrooms" required="required"  name="bathrooms" />
+                                        <input type="bathrooms" required="required"  name="bathrooms" id="bathrooms" />
                                     </div>
                                     <div className="aa-squareMeters-field">
                                         <label htmlFor="squareMeters">Square Meters </label>
-                                        <input type="squareMeters" required="required"  name="squareMeters" />
+                                        <input type="squareMeters" required="required"  name="squareMeters" id="squareMeters"/>
                                     </div>
                                     <div className="aa-yearBuilt-field">
                                         <label htmlFor="yearBuilt">Year Built </label>
-                                        <input type="yearBuilt" required="required" name="yearBuilt" />
+                                        <input type="yearBuilt" required="required" name="yearBuilt" id="yearBuilt"/>
                                     </div>
                                     <div className="aa-description-field">
                                         <label htmlFor="description">More details </label>
-                                        <input type="description" required="required"  name="description" />
+                                        <input type="description" required="required"  name="description" id="description"/>
                                     </div>
                                     <div className="aa-single-submit">
-                                        <input type="submit" value="Create" className="aa-browse-btn" name="Submit" />
+                                        <input type="submit" value="Create" className="aa-browse-btn" name="submit" id="submit"/>
                                     </div>
                                 </form>
                             </div>
