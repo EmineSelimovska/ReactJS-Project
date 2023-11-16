0471586e427bc49ed0988a11c2adbef252e9dac7
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-11-16 22:47:30 +0200

    add css for createForm label

--- my-project/src/components/Create.jsx
+++ my-project/src/components/Create.jsx
@@ -18,41 +18,41 @@ export default function Create(){
                                 <form className="createform">
                                     <div className={styles.propertyTypeField}>
                                         <label  {...styles.label} htmlFor="properyType">Property Type </label>
-                                        <input type="propertyType" required="required" name="propertyType" id="propertyType"/>
+                                        <input  type="propertyType" required="required" name="propertyType" id="propertyType"/>
                                     </div>
-                                    <div className="aa-city-field">
-                                        <label htmlFor="city" className={styles.label}>City</label>
+                                    <div className={styles.cityField}>
+                                        <label {...styles.label} htmlFor="city">City</label>
                                         <input type="city" required="required"  name="city" id="city" />
                                     </div>
-                                    <div className="aa-street-field">
-                                        <label htmlFor="street">Street </label>
-                                        <input type="street" required="required"   name="street" id="street" />
+                                    <div className={styles.streetField}>
+                                        <label {...styles.label} htmlFor="street">Street </label>
+                                        <input type="street" required="required" name="street" id="street" />
                                     </div>
-                                    <div className="aa-price-field">
-                                        <label htmlFor="price">Price </label>
+                                    <div className={styles.priceField}>
+                                        <label {...styles.label} htmlFor="price">Price </label>
                                         <input type="price" required="required"  name="price" id="price"/>
                                     </div>
-                                    <div className="aa-bedrooms-field">
-                                        <label htmlFor="bedrooms">Bedrooms </label>
+                                    <div className={styles.bedroomsField}>
+                                        <label {...styles.label} htmlFor="bedrooms">Bedrooms </label>
                                         <input type="bedrooms" required="required"  name="bedrooms" id="bedrooms"/>
                                     </div>
-                                    <div className="aa-bathrooms-field">
-                                        <label htmlFor="bathrooms">Bathrooms </label>
+                                    <div className={styles.bathroomsField}>
+                                        <label {...styles.label}  htmlFor="bathrooms">Bathrooms </label>
                                         <input type="bathrooms" required="required"  name="bathrooms" id="bathrooms" />
                                     </div>
-                                    <div className="aa-squareMeters-field">
-                                        <label htmlFor="squareMeters">Square Meters </label>
+                                    <div className={styles.squareMetersField}>
+                                        <label {...styles.label} htmlFor="squareMeters">Square Meters </label>
                                         <input type="squareMeters" required="required"  name="squareMeters" id="squareMeters"/>
                                     </div>
-                                    <div className="aa-yearBuilt-field">
-                                        <label htmlFor="yearBuilt">Year Built </label>
+                                    <div className={styles.yearBuiltField}>
+                                        <label {...styles.label} htmlFor="yearBuilt">Year Built </label>
                                         <input type="yearBuilt" required="required" name="yearBuilt" id="yearBuilt"/>
                                     </div>
-                                    <div className="aa-description-field">
-                                        <label htmlFor="description">More details </label>
+                                    <div className={styles.descriptionField}>
+                                        <label {...styles.label} htmlFor="description">More details </label>
                                         <input type="description" required="required"  name="description" id="description"/>
                                     </div>
-                                    <div className="aa-single-submit">
+                                    <div className={styles.createSubmit}>
                                         <input type="submit" value="Create" className="aa-browse-btn" name="submit" id="submit"/>
                                     </div>
                                 </form>
