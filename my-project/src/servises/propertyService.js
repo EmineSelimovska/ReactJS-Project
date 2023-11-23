37f143fce6ad585375e7176b6c6f2c435543d7af
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-11-23 16:03:15 +0200

    add request method with bind in request,json

--- my-project/src/servises/propertyService.js
+++ my-project/src/servises/propertyService.js
@@ -1,23 +1,14 @@
-import { request } from "../components/lib/request";
+import * as request from "../components/lib/request";
 
 
 const url = `http://localhost:3030/jsonstore/property`
 
 export const getAll = async () => {
-    const result = await request('GET', url);
+    const result = await request.get(url);
      return Object.values(result)
 }
+
 export const create = async (propertyDate) => {
-   const response = await fetch(`${url}`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(propertyDate)
-    })
-
-    const result = await response.json();
-
-
+    const result = await request.post(url, propertyDate);
     return result;
 }
