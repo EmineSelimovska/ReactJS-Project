6989beb61e8b875264fa229915efeefb6600b800
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-11-25 00:02:01 +0200

    add auth service
    add login redirect

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,30 +1,35 @@
+import { useState } from "react"
+import { Route, Routes, useNavigate } from "react-router-dom"
 
-import Menu from "./components/Menu-area"
+import * as authService from "./servises/authService"
+import AuthContext from "./contexts/authContext"
+
 import Properties from "./components/Properties"
-import Footer from "./components/Footer"
-import { Route, Routes } from "react-router-dom"
 import Home from "./components/Home"
 import Create from "./components/Create"
-import Header from "./components/Header"
 import Contact from "./components/Contact"
-import Blog from "./components/Blog"
 import NotFound from "./components/NotFound"
 import Login from "./components/Login"
 import Preload from "./components/Preloader"
 import Register from "./components/Register"
 import PropertyDetails from "./components/PropertyDetails"
-import { useState } from "react"
-import AuthContext from "./contexts/authContext"
+
+
+
 function App() {
+  const navigate = useNavigate()
   const [auth, setAuth] = useState({});
 
-  const loginSubmitHandler = (values) => {
-    console.log(values);
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password);
+    setAuth(result)
+
+     navigate('/');
   }
 
   return (
 
-    <AuthContext.Provider value={ {loginSubmitHandler} }>
+    <AuthContext.Provider value={{ loginSubmitHandler }}>
       <div>
         <Preload />
 
