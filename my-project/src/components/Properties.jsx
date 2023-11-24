e6b096343dd070462b76908e466cfd65a1f6fb88
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-11-24 15:39:04 +0200

    add correction in properties

--- my-project/src/components/Properties.jsx
+++ my-project/src/components/Properties.jsx
@@ -46,27 +46,7 @@ const [property, setProperty] = useState([]);
 
                  <ProperyListItem/>
 
-                  <div className="aa-properties-content-bottom">
-                    <nav>
-                      <ul className="pagination">
-                        <li>
-                          <a href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                          </a>
-                        </li>
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li className="active"><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                        <li>
-                          <a href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
+                  
                 </div>
               </div>
 
