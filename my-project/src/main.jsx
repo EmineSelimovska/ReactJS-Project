ead700a8fa6eccba5cd3d040106ae25abd677a7a
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-11-25 14:06:17 +0200

    add Toast

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <ToastContainer
+              position="bottom-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
     </BrowserRouter>
 )
