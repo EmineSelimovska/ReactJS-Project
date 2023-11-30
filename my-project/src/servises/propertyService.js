5821dc16b309b669dd65cc8e918f5ef1d564a099
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-11-30 22:45:13 +0200

    Update property json

--- my-project/src/servises/propertyService.js
+++ my-project/src/servises/propertyService.js
@@ -1,11 +1,14 @@
-import * as request from "../components/lib/request";
+import { toast } from "react-toastify";
+import * as request from "../lib/request";
 
 
 const url = `http://localhost:3030/data/property`
 
 export const getAll = async () => {
-    const result = await request.get(url);
-     return result
+   
+        const result = await request.get(url);
+        return result
+  
 }
 
 export const getOne = async (propertyId) => {
