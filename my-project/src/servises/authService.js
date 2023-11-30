dbf8f8426ea64f992ee8d50411a827f48691a616
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-12-01 00:27:10 +0200

    Update authService.js

--- my-project/src/servises/authService.js
+++ my-project/src/servises/authService.js
@@ -4,40 +4,31 @@ import { toast } from 'react-toastify';
 const baseUrl = 'http://localhost:3030/users'
 
 export const login = async (email, password) => {
-  try {
-    const result = await request.post(`${baseUrl}/login`, {
-      email,
-      password
-    });
+  const result = await request.post(`${baseUrl}/login`, {
+    email,
+    password
+   });
 
-    return result;
-  } catch (err) {
-    if (err) {
-      toast.success(err.message)
-    }
-  }
-
+   return result;
 }
 
-export const register = async (username, email, password) => {
+export const register = async (username,email, password) => {
 
-  try {
+  try{
     const result = await request.post(`${baseUrl}/register`, {
-      username,
-      email,
-      password
+     username,
+     email,
+     password
     });
     toast.success('Register Successful');
     return result;
-  } catch (err) {
-    if (err) {
-      toast.success(err.message)
-    }
+  }catch(err){
 
+   toast.error(err.message)
   }
-
+ 
 }
 
 export const logout = () => request.get(`${baseUrl}/logout`);
-
-
+ 
+ 
