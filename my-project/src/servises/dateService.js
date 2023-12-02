9fd9c58eed446ee0acbb4439eeab749e96eae795
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-12-02 13:38:43 +0200

    Update dateService.js

--- my-project/src/servises/dateService.js
+++ my-project/src/servises/dateService.js
@@ -1,20 +1,20 @@
-const baseUrl = `http://localhost:3030/jsonstore/contact`;
+// const baseUrl = `http://localhost:3030/jsonstore/contact`;
 const aboutUrl = `http://localhost:3030/data/about`;
 
-export const getContact = async () => {
-    try {
-        const response = await fetch(baseUrl);
-        const result = await response.json();
+// export const getContact = async () => {
+//     try {
+//         const response = await fetch(baseUrl);
+//         const result = await response.json();
 
-        const data = Object.values(result);
+//         const data = Object.values(result);
 
-        return data;
-    } catch (err) {
-        return err
-    }
+//         return data;
+//     } catch (err) {
+//         return err
+//     }
 
 
-}
+// }
 
 export const getAbout = async () => {
     try {
