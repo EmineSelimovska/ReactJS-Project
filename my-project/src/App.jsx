2823f08e865bef848e04b54a55194e6d186c9ff8
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-12-07 20:42:08 +0200

    Add Auth Guard

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 
-import {AuthProvider} from "./contexts/authContext"
+import { AuthProvider } from "./contexts/authContext"
 
 import Properties from "./components/properties/Properties"
 import Home from "./components/home/Home"
@@ -14,37 +14,42 @@ import PropertyDetails from "./components/property-details/PropertyDetails"
 import Logout from "./components/logout/Logout"
 import Edit from "./components/edit/Edit"
 import ErrorBoundary from "./components/ErrorBoundary"
-
+import AuthGuard from "./components/guards/AuthGuard"
 
 
 function App() {
   return (
     <ErrorBoundary>
-    <AuthProvider>
-      <div>
-        <Preload />
+      <AuthProvider>
+        <div>
+          <Preload />
 
-        <Routes>
+          <Routes>
 
-          <Route path="/" element={<Home />} />
-          <Route path="/properties" element={<Properties />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/properties/:propertyId" element={<PropertyDetails />} />
-          <Route path="/properties/:propertyId/edit" element={<Edit />} />
-
-          <Route path="/search/:searchItem" element={<Properties/>} />
-          <Route path="/logout" element={<Logout/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/properties" element={<Properties />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/properties/:propertyId" element={<PropertyDetails />} />
 
 
-          <Route path="*" element={<NotFound />} />
+            <Route path="/search/:searchItem" element={<Properties />} />
 
 
-        </Routes>
-      </div>
-    </AuthProvider>
+            <Route element={<AuthGuard />}>
+              <Route path="/create" element={<Create />} />
+              <Route path="/properties/:propertyId/edit" element={<Edit />} />
+              <Route path="/logout" element={<Logout />} />
+            </Route>
+
+
+            <Route path="*" element={<NotFound />} />
+
+
+          </Routes>
+        </div>
+      </AuthProvider>
     </ErrorBoundary>
   )
 }
