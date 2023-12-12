c30cd2450b4dc1708015a273b07291ade92909e4
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2023-12-12 13:14:58 +0200

    Add user auth guard

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -15,6 +15,7 @@ import Logout from "./components/logout/Logout"
 import Edit from "./components/edit/Edit"
 import ErrorBoundary from "./components/ErrorBoundary"
 import AuthGuard from "./components/guards/AuthGuard"
+import AuthUserGuard from "./components/guards/AuthUserGuard"
 
 
 function App() {
@@ -29,18 +30,18 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/properties" element={<Properties />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+
             <Route path="/properties/:propertyId" element={<PropertyDetails />} />
-
-
-            <Route path="/search/:searchItem" element={<Properties />} />
-
 
             <Route element={<AuthGuard />}>
               <Route path="/create" element={<Create />} />
               <Route path="/properties/:propertyId/edit" element={<Edit />} />
               <Route path="/logout" element={<Logout />} />
+            </Route>
+
+            <Route element={<AuthUserGuard />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
             </Route>
 
 
---
+++ my-project/src/components/guards/AuthUserGuard.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react"
+import AuthContext from "../../contexts/authContext"
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function AuthUserGuard(props) {
+    const { isAuthenticated } = useContext(AuthContext)
+
+    if (isAuthenticated) {
+        return <Navigate to="/" />;
+    }
+
+
+    return <Outlet />
+}
